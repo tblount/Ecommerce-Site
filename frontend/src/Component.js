cca0ed308baed0b3760ea55a0d7df62d046575a0
Terrance <tblountcs@gmail.com>
2019-07-23 16:39:40 -0500

    Uploaded files from master folder.

---
+++ frontend/src/Component.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Component.css';
+
+function Component() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+           <code>Under Constuction!</code> 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Component;
