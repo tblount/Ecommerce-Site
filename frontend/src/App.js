cacd6d0abbb080b52665e668d4d5a95877081eb9
t.blount <tblountcs@gmail.com>
2019-08-02 14:01:58 -0500

    "Updates"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link" 
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -21,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+
     </div>
   );
 }
