b4e687bd350ffe07fd6b0a23a2ae83eb1fb3e380
t.blount <tblountcs@gmail.com>
2019-08-07 23:21:30 -0500

    Updates

--- frontend/src/Component.js
+++ frontend/src/Component.js
@@ -8,7 +8,7 @@ function Component() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>Under Constuction!</code> 
+           <code> Site is still Under Constuction!</code> 
         </p>
         <a
           className="App-link"
