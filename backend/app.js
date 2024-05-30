c107612c52ff09bd1c7e514805b835b960117027
Terrance <tblountcs@gmail.com>
2024-05-29 19:27:48 -0500

    updates

--- backend/app.js
+++ backend/app.js
@@ -38,4 +38,18 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// backend/app.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
 module.exports = app;
